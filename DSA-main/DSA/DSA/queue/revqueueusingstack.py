243da36096b0588359b7fe118273c7504b77456a
sowji32 <koppulalakshmisowjanya@gmail.com>
2025-11-08 18:36:01 +0530

    Create revqueueusingstack.py

---
+++ DSA-main/DSA/DSA/queue/revqueueusingstack.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+def reverse_queue(q):
+    stack = []
+    while q:
+        stack.append(q.popleft())
+    while stack:
+        q.append(stack.pop())
+    return q
+
+q = deque([1, 2, 3, 4, 5])
+print(reverse_queue(q)) 
