09bc56ea3c76c9610356567d062142e7caf7ca21
23A31A0514 <koppulalakshmisowjanya@gmail.com>
2025-12-19 18:06:32 +0530

    Implement basic Graph class with vertices and edges

---
+++ graphs/insert.py
@@ -0,0 +1,19 @@
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_vertex(self, v):
+        if v not in self.graph:
+            self.graph[v] = []
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.add_vertex(u)
+        if v not in self.graph:
+            self.add_vertex(v)
+        self.graph[u].append(v)
+        self.graph[v].append(u)   # Undirected graph
+
+    def display(self):
+        for vertex in self.graph:
+            print(vertex, "->", self.graph[vertex])
